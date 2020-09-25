e868457569d0b4c7ed5385da9e004755b2f7e87c
Avinash Singh <avi18042001singh@gmail.com>
2020-09-25 15:45:16 -0400

    Random quote bot is now priting two quotes

--- get-quote.py
+++ get-quote.py
@@ -9,7 +9,12 @@ def primary():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  # rmvd = quotes.remove(quotes[rnd])
+
+  last = len(quotes) - 1
+  second_rnd = random.randint(0, last)
+  print(quotes[rnd], end = "")
+  print(quotes[second_rnd])
 
 if __name__== "__main__":
   primary()
